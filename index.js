a3abfa57139308411f987521aadc813affd63667
Miguel Martínez <leugimmanre@gmail.com>
2023-02-24 21:00:19 +0100

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const Hola = document.addEventListener('click', llamarHola.llamarHola = () => console.log('Hola'));
+
+
+function llamarHola() {
+    console.log('Hola')
+}
+
+
+})
